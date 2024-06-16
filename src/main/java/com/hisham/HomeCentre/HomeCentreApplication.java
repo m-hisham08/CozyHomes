f88dada068ccc851b22d2c4c74f4b7d1d8b699a3
m-hisham08 <mohammadhisham852@gmail.com>
2024-06-16 11:06:27 +0530

    initialized git repository

---
+++ src/main/java/com/hisham/HomeCentre/HomeCentreApplication.java
@@ -0,0 +1,13 @@
+package com.hisham.HomeCentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeCentreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hisham/HomeCentre/HomeCentreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hisham.HomeCentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCentreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
